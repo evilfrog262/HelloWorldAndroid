573e3a285547a59222c854bb74683fa8e28313be
krcox <kristincox09@gmail.com>
2016-01-03 15:50:46 -0800

    Test commit

--- app/src/main/java/com/helloworld/krcox/helloworld/HelloWorld.java
+++ app/src/main/java/com/helloworld/krcox/helloworld/HelloWorld.java
@@ -1,13 +1,21 @@
 package com.helloworld.krcox.helloworld;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class HelloWorld extends AppCompatActivity {
+
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        String str = "";
     }
 }
